adfea6d657eafcc883d1a77d6c80072a9cc2d2d8
vickybharath410 <vickybharath410@gmail.com>
2023-02-25 16:58:15 +0530

    file updated

--- src/components/Viewpage.js
+++ src/components/Viewpage.js
@@ -5,6 +5,7 @@ import "./View.css"
 function Viewpage() {
   const[details,setDetails]=useState([]);
   const[search,setSearch]=useState("");
+  const[searchDetails,setSearchDetails]=useState([]);
   const navigate=useNavigate();
 
   useEffect(()=>{
@@ -12,6 +13,11 @@ function Viewpage() {
     .then(res=>setDetails(res.data.details))
     .catch(e=>console.log(e))
   },[])
+
+  function searchItems(){
+      let datas=details.filter(data=>data.title.includes(search));
+      setSearchDetails(datas)
+  }
 
   return (
     <div className='container'>
@@ -21,14 +27,19 @@ function Viewpage() {
           alt="logo"
           src="https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg"
         />
-       <button type="button" className="btn btn-primary">LogOff</button>
+       <button onClick={()=>{
+        localStorage.clear()
+        navigate("/")
+       }}  type="button" className="btn btn-primary">LogOff</button>
       </div>
       <div className="input-group mb-3">
   <input type="text" value={search} onChange={e=>setSearch(e.target.value)} className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-  <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+  <button onClick={()=>searchItems()} className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
 </div>
 <div><button onClick={()=>navigate("/addnew")}>Add New</button></div>
-   <div className='view'>
+   <div>
+    {searchDetails.length===0?<>
+      <div className='view'>
    {details.map((data,index)=>{
     
       return <div onClick={()=>navigate("/details",{state:data})
@@ -40,6 +51,23 @@ function Viewpage() {
     </div>
         
     })}
+   </div>
+    </>:<>
+    <div className='view'>
+   {searchDetails.map((data,index)=>{
+    
+      return <div onClick={()=>navigate("/details",{state:data})
+  } style={{backgroundImage:'url('+data.image.url+')',width:"450px",height:"300px",backgroundSize:"100% 100%",marginBottom:"15px"}}>
+        {console.log(data)}
+        <span style={{width:"400px",backgroundColor:'black',height:"40px",color:"yellow",fontWeight:"bolder",fontSize:"larger"}}>
+      {data.title}
+        </span>
+    </div>
+        
+    })}
+   </div>
+    </>}
+   
    </div>
     </div>
   )
